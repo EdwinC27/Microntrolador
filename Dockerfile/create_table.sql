ebfe75b6110d0a9154497588521b7961165857e4
Edwin Alejandro <Edwin_Alejandro@gmail.com>
2023-02-27 16:47:51 -0600

    Implementar necesario para docker

---
+++ Dockerfile/create_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE informacion_generada (
+    hora character varying NOT NULL,
+    genero character varying,
+    ciudad character varying,
+    clima character varying,
+    canciones character varying
+);
