1c0edf11245dc9bc7b98920e3994bc7e5f16a308
EdwinC27 <edwin.a.castillo.27@gmail.com>
2023-03-10 22:01:38 -0600

    Agregar la tabla del usuario con un registro

--- dockerDir/create_table.sql
+++ dockerDir/create_table.sql
@@ -5,3 +5,13 @@ CREATE TABLE informacion_generada (
     clima character varying,
     canciones character varying
 );
+
+CREATE TABLE users (
+    users character varying,
+    password character varying,
+    token character varying
+);
+
+INSERT INTO public.users(
+	users, password, token)
+	VALUES ('edwin', '20254820b2c8ea8a3ff8705e46509a2bf4fd9a6b8243e64b7dabe118f8ff7639', '');
