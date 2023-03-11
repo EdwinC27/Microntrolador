02c950a30fc6f4fb7687de3c7a749d53ffd32e29
Edwin Alejandro Castillo <93620716+EdwinC27@users.noreply.github.com>
2023-03-11 12:28:27 -0600

    Tabla user donde se van a guardar las credenciales

--- Base_Datos/schemaAPI.sql
+++ Base_Datos/schemaAPI.sql
@@ -12,3 +12,13 @@ ALTER TABLE ONLY public.informacion_generada
     ADD CONSTRAINT informacion_generada_pkey PRIMARY KEY (hora);
 
 
+CREATE TABLE users (
+    users character varying,
+    password character varying,
+    token character varying
+);
+
+
+INSERT INTO public.users(
+	users, password, token)
+	VALUES ('edwin', '20254820b2c8ea8a3ff8705e46509a2bf4fd9a6b8243e64b7dabe118f8ff7639', '');
